de95d7695e884179ba260f86b11d27da91258ec9
MrLiteng4552 <randoomchik6@gmail.com>
2026-06-11 18:12:16 +0300

    Update Department.cs

--- ContactManager/Models/Department.cs
+++ ContactManager/Models/Department.cs
@@ -7,6 +7,5 @@ public class Department
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
 
-    // Связь: у одного отдела может быть много контактов
     public virtual ICollection<Contact> Contacts { get; set; } = new List<Contact>();
 }
